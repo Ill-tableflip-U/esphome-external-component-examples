6366ba3a4b1c3fe98e4b05206c1b5a89de5f49bd
Ill-tableflip-U <125164221+Ill-tableflip-U@users.noreply.github.com>
2024-09-23 23:55:34 +1000

    Update empty_uart_component.h

--- components/empty_uart_component/empty_uart_component.h
+++ components/empty_uart_component/empty_uart_component.h
@@ -5,7 +5,16 @@
 
 namespace esphome {
 namespace empty_uart_component {
+#include <esphome/core/types.h>
 
+namespace esphome {
+
+  class MyComponentEvent : public Event {
+  public:
+    // Optional properties or methods for the event
+  };
+
+}
 class EmptyUARTComponent : public uart::UARTDevice, public Component {
   public:
     void setup() override;
